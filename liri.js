8c6ed645b50b6c3c0502495fa9cff061fc2aa3f3
Craig Bomba <talk2cbo@gmail.com>
2018-08-01 15:39:06 -0500

    small changes to spotify search call

--- liri.js
+++ liri.js
@@ -1,23 +1,37 @@
 require("dotenv").config();
 var keys = require('./keys');
-// import keys from './keys';
 console.log("the value of keys is: ", keys);
 console.log("the value of twitter keys is: ", keys.twitter);
+
+
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+
 var Twitter = require('twitter');
 // var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 console.log(client);
 
-client.get('search/tweets', {q: 'cally_bomba'}, function(error, tweets, response) {
-  for (i = 0; i < tweets.length; i++) {
-    console.log(tweets[i].text);
+client.get('search/tweets', { q: 'cally_bomba', count: '20' }, function (error, tweets, response) {
+  console.log("=========================================================================================");
+  console.log("the length of statuses is: ", tweets.statuses.length);
+  for (i = 0; i < tweets.statuses.length; i++) {
+    console.log(tweets.statuses[i].text);
   }
+  console.log("=========================================================================================");
   // console.log(tweets);
 });
 
-// client.get('favorites/list', function(error, tweets, response) {
-//   if(error) throw error;
-//   console.log(tweets);  // The favorites.
-//   console.log(response);  // Raw response object.
-// });
+
+// spotify stuff...
+console.log("------------ spotify ----------------");
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function (response) {
+    console.log(JSON.stringify(response));
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
